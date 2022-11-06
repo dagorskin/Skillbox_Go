99a5b1c3e73b82aa3336d979e23cc5d308203049
Dmitriy Gorskin <dagorskin@gmail.com>
2022-11-06 21:39:35 +0300

    Done task 5 for module #6

---
+++ module6/src/task5.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("------------------------------")
+	fmt.Println("Задание 5 (по желанию). Задача на постепенное наполнение корзин разной ёмкости (if и continue и break).")
+	fmt.Println("------------------------------")
+
+	var firstBasketLimit, secondBasketLimit, thirdBasketLimit, firstBasket, secondBasket, thirdBasket, basket, fullBasket int
+
+	// Ввод даных.
+	fmt.Print("Введите ёмкость трёх корзин одной строкой через пробел: ")
+	fmt.Scan(&firstBasketLimit, &secondBasketLimit, &thirdBasketLimit)
+
+	// Заполнение корзин.
+	for {
+		basket++
+		// Первая корзина.
+		if firstBasketLimit == basket {
+			firstBasket = basket
+			fullBasket++
+			continue
+		}
+		// Вторая корзина.
+		if secondBasketLimit == basket {
+			secondBasket = basket
+			fullBasket++
+			continue
+		}
+		// Третья корзина.
+		if thirdBasketLimit == basket {
+			thirdBasket = basket
+			fullBasket++
+			continue
+		}
+		// Проверка на количество заполненых корзин.
+		if fullBasket == 3 {
+			break
+		}
+	}
+
+	fmt.Print("Первая корзина заполнена на ", firstBasket, ", вторая на ", secondBasket, " и третья на ", thirdBasket, " яблок.")
+}
