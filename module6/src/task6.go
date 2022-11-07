ae24b44392bd28da02df314ebcf52b691e660cc8
Dmitriy Gorskin <dagorskin@gmail.com>
2022-11-07 17:27:08 +0300

    Done all tasks for module #6 :)

--- module6/src/task6.go
+++ module6/src/task6.go
@@ -7,24 +7,20 @@ func main() {
 	fmt.Println("Задание 6 (по желанию). Движение лифта.")
 	fmt.Println("------------------------------")
 
+	// Инициализация заведомо известных данных.
 	const groundFloor, lastFloor, weightLimit = 1, 24, 2
-	var people []int
+	var floors []int
 	elevatorPosition, numberPeopleInElevator, quantityPeopleGroundFloor, quantityPeopleTheFloors := 1, 0, 0, 0
 
+	// Основной цикл работы программы.
 	for {
+		elevatorPosition = groundFloor
 		fmt.Println("Лифт на", elevatorPosition, "этаже.")
 
-		if elevatorPosition == 1 {
+		// Ввод данных о людях нуждающихся в услугах лифта.
+		if numberPeopleInElevator == 0 && quantityPeopleGroundFloor == 0 && quantityPeopleTheFloors == 0 {
 			fmt.Print("Сколько человек хотят воспользоваться лифтом на первом этаже? ")
 			fmt.Scan(&quantityPeopleGroundFloor)
-
-			// Посадка пассажиров в лифт.
-			if quantityPeopleGroundFloor <= 2 {
-				numberPeopleInElevator = quantityPeopleGroundFloor
-			} else {
-				numberPeopleInElevator += 2
-				quantityPeopleGroundFloor -= 2
-			}
 
 			fmt.Print("Сколько человек хотят воспользоваться лифтом других этажах? ")
 			fmt.Scan(&quantityPeopleTheFloors)
@@ -35,16 +31,49 @@ func main() {
 				for i := quantityPeopleTheFloors; i > 0; i-- {
 					var tempPeople int
 					fmt.Scan(&tempPeople)
-					people = append(people, tempPeople)
+					floors = append(floors, tempPeople)
 				}
 			}
-			// Движение вверх.
-			if quantityPeopleGroundFloor > 0 {
-				fmt.Println("Лифт движется наверх развозить людей.")
-				elevatorPosition = 24
-				fmt.Println("Лифт на", elevatorPosition, "этаже.")
-				numberPeopleInElevator = 0
-				fmt.Print("В лифте осталось ", numberPeopleInElevator, ", а на первом этаже ", quantityPeopleGroundFloor, ".\n")
+		}
+
+		// Посадка пассажиров в лифт.
+		numberPeopleInElevator = 0
+		if quantityPeopleGroundFloor >= weightLimit {
+			numberPeopleInElevator += 2
+			quantityPeopleGroundFloor -= 2
+		} else if quantityPeopleGroundFloor == 0 && quantityPeopleTheFloors == 0 {
+			fmt.Print("В лифте осталось ", numberPeopleInElevator, ", на первом этаже ", quantityPeopleGroundFloor, ".\n")
+			fmt.Println("Лифт развёз всех людей!")
+			break
+		} else {
+			numberPeopleInElevator = quantityPeopleGroundFloor
+			quantityPeopleGroundFloor = 0
+		}
+
+		// Движение вверх.
+		if quantityPeopleGroundFloor > 0 || quantityPeopleTheFloors > 0 {
+			fmt.Print("В лифт загрузилось ", numberPeopleInElevator, " человек, осталось на первом этаже ", quantityPeopleGroundFloor, ".\n")
+			fmt.Println("Лифт движется наверх.")
+			elevatorPosition = lastFloor
+			fmt.Println("Лифт на", elevatorPosition, "этаже. Все кто были в лифте вышли.")
+			numberPeopleInElevator = 0
+			fmt.Print("В лифте осталось ", numberPeopleInElevator, ", на первом этаже ", quantityPeopleGroundFloor, ".\n")
+		}
+
+		// Движение вниз.
+		fmt.Println("Лифт пустой и движется вниз.")
+		for quantityPeopleTheFloors > 0 {
+			elevatorPosition = floors[quantityPeopleTheFloors-1]
+			quantityPeopleTheFloors--
+			numberPeopleInElevator++
+			floors = append(floors[:quantityPeopleTheFloors], floors[quantityPeopleTheFloors+1:]...)
+			fmt.Println("Лифт остановился на", elevatorPosition, "этаже, забрал человека и поехал дальше.")
+			fmt.Println("В лифте теперь:", numberPeopleInElevator, "человек.")
+
+			if numberPeopleInElevator == 2 {
+				fmt.Println("Лифт полон и движется вниз не подбирая никого.")
+				fmt.Println("---------------------------------------------")
+				break
 			}
 		}
 	}
