7790fc0219b865a1fd92b959a254c0a5bcfa2fc8
Dmitriy Gorskin <dagorskin@gmail.com>
2022-11-15 21:02:20 +0300

    Done module #9

--- module9/src/task1.go
+++ module9/src/task1.go
@@ -1,9 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func task1() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 1. Переполнение.")
 	fmt.Println()
+
+	// Инициализация.
+	var (
+		overflowUnit8Count  int
+		overflowUnit16Count int
+		varUint8            uint8  = math.MaxUint8
+		varUint16           uint16 = math.MaxUint16
+	)
+
+	// Подсчет количества переполнений.
+	for varUint64 := 0; varUint64 != math.MaxUint32; varUint64++ {
+		if varUint8 == 0 {
+			overflowUnit8Count++
+		}
+
+		if varUint16 == 0 {
+			overflowUnit16Count++
+		}
+		varUint8--
+		varUint16--
+	}
+
+	// Вывод результатов.
+	fmt.Printf("Количество переполнений чисел типа uint8: %d, чисел типа uint16: %d;\n", overflowUnit8Count, overflowUnit16Count)
+
 }
--- module9/src/task2.go
+++ module9/src/task2.go
@@ -1,9 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// Функция-шаблон вывода результата.
+func printResult(multiplicationInputNumbers int, resultType string) {
+	_, _ = fmt.Printf("Для произведения этих чисел (%d) подойдет тип <%s>.\n", multiplicationInputNumbers, resultType)
+}
 
 func task2() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 2. Минимальный тип данных.")
 	fmt.Println()
+
+	// Инициализация.
+	var (
+		inputNumberOne             int
+		inputNumberTwo             int
+		multiplicationInputNumbers int
+	)
+
+	// Ввод данных.
+	fmt.Print("Введите первое число: ")
+	_, _ = fmt.Scan(&inputNumberOne)
+	fmt.Print("Введите второе число: ")
+	_, _ = fmt.Scan(&inputNumberTwo)
+
+	multiplicationInputNumbers = inputNumberOne * inputNumberTwo
+
+	// Поиск нужного типа.
+	switch {
+	case 0 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxUint8:
+		printResult(multiplicationInputNumbers, "uint8")
+	case 0 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxUint16:
+		printResult(multiplicationInputNumbers, "uint16")
+	case 0 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxUint32:
+		printResult(multiplicationInputNumbers, "uint32")
+	case 0 <= multiplicationInputNumbers:
+		printResult(multiplicationInputNumbers, "uint64")
+	case math.MinInt8 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxInt8:
+		printResult(multiplicationInputNumbers, "int8")
+	case math.MinInt16 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxInt16:
+		printResult(multiplicationInputNumbers, "int16")
+	case math.MinInt32 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxInt32:
+		printResult(multiplicationInputNumbers, "int32")
+	case math.MinInt64 <= multiplicationInputNumbers && multiplicationInputNumbers < math.MaxInt64:
+		printResult(multiplicationInputNumbers, "int64")
+	}
 }
