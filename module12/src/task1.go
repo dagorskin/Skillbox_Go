f97b0758625a3f826a661c5049b6572eae5f72ee
Dmitriy Gorskin <dagorskin@gmail.com>
2022-12-03 18:19:11 +0300

    Module 12 is done

--- module12/src/task1.go
+++ module12/src/task1.go
@@ -8,7 +8,6 @@ import (
 	"time"
 )
 
-// task1 Основная функция.
 func task1() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 1. Работа с файлами.")
@@ -22,12 +21,17 @@ func task1() {
 	)
 
 	file, err := os.OpenFile("file.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Print("Файл не создан! Ошибка: ")
+		fmt.Println(err)
+	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(os.Stdin)
 
-	for scanner.Scan() {
+	for dataInput != "exit" {
 		fmt.Printf("Введите какой-либо произвольный текст (exit = выход): ")
+		scanner.Scan()
 		dataInput = scanner.Text()
 
 		if dataInput == "exit" {
@@ -37,14 +41,12 @@ func task1() {
 		}
 		timeNow := time.Now().Format("2006-01-02 15:04:05")
 		dataWrite = strconv.Itoa(int(numberString)) + ") " + timeNow + " " + dataInput + "\n"
-		if err != nil {
-			fmt.Println("Файл не создан! Повторите.")
-			continue
-		}
 
 		if _, err = file.WriteString(dataWrite); err != nil {
-			panic(err)
+			fmt.Print("Ошибка при попытке записи: ")
+			fmt.Println(err)
 		}
 		fmt.Println("Запись прошла успешно...")
+
 	}
 }
--- module12/src/task5.go
+++ module12/src/task5.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
+
+func generatingBrackets(openBrackets, closedBrackets int, stringBrackets string, arrayBrackets *[]string) {
+	if openBrackets == 0 {
+		*arrayBrackets = append(*arrayBrackets, stringBrackets+strings.Repeat(")", closedBrackets))
+		return
+	}
+	if closedBrackets > openBrackets {
+		generatingBrackets(openBrackets, closedBrackets-1, stringBrackets+")", arrayBrackets)
+	}
+	generatingBrackets(openBrackets-1, closedBrackets, stringBrackets+"(", arrayBrackets)
+}
 
 func task5() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 5 (по желанию). Комбинации круглых скобок.")
 	fmt.Println()
+
+	file, err := os.OpenFile("task5.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Print("Файл не создан! Ошибка: ")
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var quantityBrackets uint16
+	fmt.Print("Введите количество пар скобок: ")
+	_, _ = fmt.Scan(&quantityBrackets)
+
+	closedBrackets, openBrackets := quantityBrackets, quantityBrackets
+	var stringBrackets string
+	arrayBrackets := make([]string, 0)
+
+	generatingBrackets(int(closedBrackets), int(openBrackets), stringBrackets, &arrayBrackets)
+
+	if _, err = file.WriteString(strings.Join(arrayBrackets, " ")); err != nil {
+		fmt.Print("Ошибка при попытке записи: ")
+		fmt.Println(err)
+	}
+
+	fmt.Println("Результат записан:\n", arrayBrackets)
+
 }
