1cb122527161a9bffd180ab0c3f33ee57cc1872b
Dmitriy Gorskin <dagorskin@gmail.com>
2022-12-09 20:30:57 +0300

    Module #15 is done!

--- module15/src/task2.go
+++ module15/src/task2.go
@@ -4,8 +4,42 @@ import (
 	"fmt"
 )
 
+func inputNumbersNew() ([]int, int) {
+	var countNumbers int
+	fmt.Print("Введите количество целых чисел (max 100): ")
+	_, err := fmt.Scan(&countNumbers)
+	if err != nil {
+		fmt.Print("Ошибка ввода: ")
+		fmt.Println(err)
+	}
+
+	var numbersArr []int
+	fmt.Printf("Введите %v чисел по порядку:\n", countNumbers)
+	var num int
+	for i := 0; i < countNumbers; i++ {
+		_, err = fmt.Scan(&num)
+		if err != nil {
+			fmt.Print("Ошибка ввода: ")
+			fmt.Println(err)
+		}
+		numbersArr = append(numbersArr, num)
+	}
+	return numbersArr, countNumbers
+}
+
+func reverse(inputArr []int, countNumbers int) []int {
+	var reverseArr []int
+	for i := countNumbers - 1; i >= 0; i-- {
+		reverseArr = append(reverseArr, inputArr[i])
+	}
+	return reverseArr
+}
+
 func task2() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 2. Функция, реверсирующая массив.")
 	fmt.Println()
+
+	normalArr, countNumbers := inputNumbersNew()
+	fmt.Printf("Массив в первичном порядке: %v\nМассив в обратном порядке: %v\n", normalArr, reverse(normalArr, countNumbers))
 }
