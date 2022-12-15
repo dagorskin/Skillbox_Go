a1e5c4c0cca516ad6fc83284d6fe513c0fc996de
Dmitriy Gorskin <dagorskin@gmail.com>
2022-12-16 01:19:01 +0300

    Task #1 is done!

--- module21/src/task1.go
+++ module21/src/task1.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func task1() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 1. Расчёт по формуле.")
 	fmt.Println()
+
+	var (
+		S float32
+		x int16
+		y uint8
+		z float32
+	)
+
+	S = float32(2) * float32(x) * float32(math.Pow10(int(y))) - float32(3)/z
+
+	fmt.Println("Результат:", S)
 }
