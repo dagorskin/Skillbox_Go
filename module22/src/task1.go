23304cf06660255c8164a6b57c0aa170505dceee
Dmitriy Gorskin <dagorskin@gmail.com>
2022-12-17 01:01:39 +0300

    Module #22 is done!

--- module22/src/task1.go
+++ module22/src/task1.go
@@ -1,9 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const length = 10
+
+// Создание массива из оригинальных случайных цифр.
+func generationArrUnsorted() (array [length] int) {
+	rand.Seed(time.Now().UnixNano())
+	var tempNumber, count int
+	for index := 0; index < length; index++ {
+		tempNumber = rand.Intn(length+(length*0.5))
+		for _, number := range array {
+			if number == tempNumber {
+				count++
+			}
+		}
+		if count == 0 {
+			array[index] = tempNumber
+		} else {
+			index--
+		}
+		count = 0
+	}
+	return
+}
+
+// Поиск числа и вывод результата.
+func numberSearch(array[length] int, desiredNumber int) (result int){
+	for index, number := range array {
+		if number == desiredNumber {
+			result = length-index-1
+			break
+		}
+	}
+	return
+}
 
 func task1() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 1. Подсчёт чисел в массиве.")
 	fmt.Println()
+
+	var desiredNumber, result int
+	fmt.Print("Введите число: ")
+	_, _ = fmt.Scan(&desiredNumber)
+
+	array := generationArrUnsorted()
+	result = numberSearch(array, desiredNumber)
+	fmt.Printf("Используемый массив:\n%v\n", array)
+	fmt.Printf("Результат: %v\n", result)
 }
--- module22/src/task2.go
+++ module22/src/task2.go
@@ -1,9 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const lengthNew = 12
+
+func generationArrSorted()(array [lengthNew] int) {
+	rand.Seed(time.Now().UnixNano())
+	var tempNumber, count, randomNumber int
+	randomNumber = 3
+	for index1 := 0; index1 < lengthNew; index1++ {
+		tempNumber = rand.Intn(randomNumber-tempNumber) + tempNumber
+		for index2, _ := range array {
+			if array[index2] > tempNumber {
+				count++
+			}
+		}
+		if count == 0 {
+			array[index1] = tempNumber
+			randomNumber += 2
+		} else {
+			index1--
+		}
+		count = 0
+	}
+	return
+}
+
+func numberSearchNew(array[lengthNew]int, desiredNumberNew int) (result int) {
+	result = -1
+	for index, number := range array {
+		if number == desiredNumberNew {
+			result = index
+			break
+		}
+	}
+	return
+}
 
 func task2() {
 	fmt.Println("------------------------------")
 	fmt.Println("Задание 2. Нахождение первого вхождения числа в упорядоченном массиве (числа могут повторяться).")
 	fmt.Println()
+
+	var desiredNumberNew, result int
+
+	fmt.Print("Введите число: ")
+	_, _ = fmt.Scan(&desiredNumberNew)
+
+	array := generationArrSorted()
+	result = numberSearchNew(array, desiredNumberNew)
+
+	fmt.Printf("Используемый массив:\n%v\n", array)
+	if result == -1 {
+		fmt.Println("Такого числа нет в массиве!")
+	} else {
+		fmt.Printf("Индекс: %v\n", result)
+	}
 }
