c1b5c9918f00ba7cfa0221a9f5a6972db6e68318
Dmitriy Gorskin <dagorskin@gmail.com>
2022-12-27 19:21:54 +0300

    Module #24 Update!

--- module24/src/task2.go
+++ module24/src/task2.go
@@ -10,7 +10,7 @@ func finding(sentences [4]string, chars [5]rune) (result [4][5]int) {
 		for indexWord, word := range letter {
 			for indexRune, char := range chars {
 				if strings.ToLower(string(char)) == strings.ToLower(string(word)) {
-					result[indexString][indexRune] = indexWord
+					result[indexString][indexRune] = indexWord + 1
 				}
 			}
 		}
@@ -26,6 +26,43 @@ func task2() {
 	sentences := [4]string{"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
 	chars := [5]rune{'H', 'E', 'L', 'П', 'М'}
 
-	fmt.Println(finding(sentences, chars))
+	result := finding(sentences, chars)
+	fmt.Println("Результат поиска букв в строках:\n", result)
+
+	fmt.Println("\nРезультат первого элемента:")
+	for i := 0; i != 5; i++ {
+		if result[0][i] == 0 {
+			continue
+		} else {
+			fmt.Printf("%q позиция %d\n", chars[i], result[0][i]-1)
+		}
+	}
+
+	fmt.Println("\nРезультат второго элемента:")
+	for i := 0; i != 5; i++ {
+		if result[1][i] == 0 {
+			continue
+		} else {
+			fmt.Printf("%q позиция %d\n", chars[i], result[1][i]-1)
+		}
+	}
+
+	fmt.Println("\nРезультат третьего элемента:")
+	for i := 0; i != 5; i++ {
+		if result[2][i] == 0 {
+			continue
+		} else {
+			fmt.Printf("%q позиция %d\n", chars[i], result[2][i]-1)
+		}
+	}
+
+	fmt.Println("\nРезультат четвертого элемента:")
+	for i := 0; i != 5; i++ {
+		if result[3][i] == 0 {
+			continue
+		} else {
+			fmt.Printf("%q позиция %d\n", chars[i], result[3][i]-1)
+		}
+	}
 
 }
